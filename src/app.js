f510c0fb9d083539614da92a0f8bd35cfe12615f
Aszmel <aszmel@o2.pl>
2018-11-04 22:17:42 +0100

    first commit boilerplate of Expensify

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const ExpenseDashboardPage = () => (
+  <div>This is from my ExpenseDashboardPage</div>
+);
+
+const AddExpensePage = () => <div>This is from my AddExpensePage</div>;
+
+const EditExpansePage = () => <div>This is from my EditExpansePage</div>;
+
+const HelpPage = () => <div>This is from my HelpPage</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpansePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
